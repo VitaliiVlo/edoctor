2f7beb5241f7a5afacaf233559c1aa339ad5f28b
vitalii.vloshchynskiy <vitalii.vloshchynskyi@vakoms.com.ua>
2019-04-24 14:29:15 +0300

    Update admin configuration

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from main.models import Hospital, UserProfile, Visit
 
@@ -10,13 +10,23 @@ class MyUserChangeForm(UserChangeForm):
         model = UserProfile
 
 
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = UserProfile
+
+
+ADDITIONAL_FIELDS_USER = (
+    ('Personal info', {'fields': ('gender', 'role', 'phone_number', 'birthday',
+                                  'city', 'street', 'zip_code', 'hospital')}),
+)
+
+
 class MyUserAdmin(UserAdmin):
     form = MyUserChangeForm
+    add_form = MyUserCreationForm
 
-    fieldsets = UserAdmin.fieldsets + (
-        ('Personal info', {'fields': ('gender', 'role', 'phone_number', 'birthday', 'city', 'street', 'zip_code',
-                                      'hospital')}),
-    )
+    fieldsets = UserAdmin.fieldsets + ADDITIONAL_FIELDS_USER
+    add_fieldsets = UserAdmin.add_fieldsets + ADDITIONAL_FIELDS_USER
 
 
 admin.site.register(UserProfile, MyUserAdmin)
